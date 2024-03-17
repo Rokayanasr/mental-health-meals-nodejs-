27ee6dba9a3c0f6811d2677570ffd861e7ba04ff
Rokaya Nasr <rokanasr60@gmail.com>
2024-03-17 14:44:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ mongoose.connect("mongodb://localhost:27017/Healthy").then(()=>{
 })
 
 app.use(cors({
-    origin: ['http://localhost:5173' ]
+    origin: ['http://localhost:5173' , 'http://localhost:4200' ]
   }));
 // app.use(express.static('public'));
 app.use("/uploads" , express.static('uploads'))
